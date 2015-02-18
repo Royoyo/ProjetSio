42c376b7c4d1f1e4e3ac806630375e7127c52652
nicolasdeu <nicolas.deutschmann@abase.fr>
2015-02-18 08:18:21 +0100

    suppression des partie non code

--- ProjetSIO/SQL/planning_scolaire.sql
+++ ProjetSIO/SQL/planning_scolaire.sql
@@ -1,23 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Client :  127.0.0.1
--- Généré le :  Mar 17 Février 2015 à 19:18
--- Version du serveur :  5.6.21
--- Version de PHP :  5.6.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données :  `planning_scolaire`
+﻿-- Base de données :  `planning_scolaire`
 --
 
 -- --------------------------------------------------------
@@ -139,7 +120,3 @@ ADD CONSTRAINT `demiejourneedispo_ibfk_1` FOREIGN KEY (`IdDemieJournee`) REFEREN
 --
 ALTER TABLE `semaine`
 ADD CONSTRAINT `semaine_ibfk_1` FOREIGN KEY (`IdAnnee`) REFERENCES `anneescolaire` (`Id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
