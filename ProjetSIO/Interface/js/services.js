d1b655e83920db72b9524aa9edf79f30dca8ef3a
Guillaume Haag <haag.guillaume@gmail.com>
2015-09-25 20:27:29 +0200

    formatage + ajout service PlanService

--- ProjetSIO/Interface/js/services.js
+++ ProjetSIO/Interface/js/services.js
@@ -5,19 +5,38 @@
 // Bien sûr toutes les fonctions pour la connection avec la BDD ne sont pas encore en place dans cet exemple
 // et le système sera bien différent une fois l'api backend mise en place
 // TO DO : remplacer les infos de la maquette et mettre une connexion avec l'API backend
-webApp.service('adminList',
-	function($filter, Restangular){
+webApp.service('adminService',
+	function(Restangular){
 
 		var personnes = Restangular.all('admin/personnes');
+		
+
+		//Une fonction pour récupérer la liste entière
+		var getPersonnes = function(){
+			return personnes;
+		}
+		
+		//Une fonction pour récupérer un user en particulier
+		var getPersonne = function(id) {
+			return personnes.get(id);
+		}
+		
+		
+		return {
+			//De base, les fonctions déclarées dans un service sont privées, pour les rendre publiques il faut les indiquer comme cela :
+			getPersonnes : getPersonnes,
+			getPersonne : getPersonne
+		}
+	})
+	
+webApp.service('planService',
+	function(Restangular){
+
+		var personnes = Restangular.all('plan/personnes');
 		
 			
 		
 		//Toute la logique objet que j'indique ici sera côté serveur à terme
-		
-		//Une fonction pour ajouter un user à la liste
-		var addPersonne = function(newObj) {
-			personnes.push(newObj);
-		}
 		
 		//Une fonction pour récupérer la liste entière
 		var getPersonnes = function(){
@@ -32,8 +51,16 @@ webApp.service('adminList',
 		
 		return {
 			//De base, les fonctions déclarées dans un service sont privées, pour les rendre publiques il faut les indiquer comme cela :
-			addPersonne : addPersonne,
 			getPersonnes : getPersonnes,
 			getPersonne : getPersonne
+		}
+	})
+	
+webApp.service('loginService',
+	function(Restangular){
+		
+		keep
+		//TO DO
+		return {
 		}
 	})
