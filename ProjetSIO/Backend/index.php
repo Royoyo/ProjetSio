90d804028db7905804705c01c529fdc664735df2
sinoan <spielmann.romain@orange.fr>
2015-11-05 17:23:55 +0100

    Login fonctionnel

--- ProjetSIO/Backend/index.php
+++ ProjetSIO/Backend/index.php
@@ -1,48 +1,58 @@
 <?php
 require 'vendor/autoload.php';
 require_once 'model.php';
-
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);    
 $app = new \Slim\Slim();
 
-$app->get('/', function () {
-    echo "Hello, it works!";
-});
-
-$app->post('/login', function () use ($app) {
-    $user = array(
-    "name" => "Guillaume",
-    "password" => "123",
-    "userRole"   => "administrateur"
-    );
-    $app->response->headers->set('Content-Type', 'application/json');
-    $app->response->setBody(json_encode($user));
-});
-
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
-
+// Authentification
 function authenticate(\Slim\Route $route) {
     $app = \Slim\Slim::getInstance();
-    $uid = $app->getEncryptedCookie('uid');
-    $key = $app->getEncryptedCookie('key');
-    if (validateUserKey($uid, $key) === false) {
+    if (!isset($_SESSION['token']) && !isset($_SESSION['id'])) {
       $app->halt(401);
     }
 }
 
+$app->post('/login', function () use ($app) {
+    $request = $app->request();
+    $json = $app->request->getBody();
+    $data = json_decode($json, true);
+    $username = $data['name'];
+    $password = $data['password'];
+    $token = "demo";
+    $user_obj = Users::whereRaw('login = ? and password = ?', [$username, $password])->with('roles')->firstOrFail();
+
+    // Créer session
+    if (!isset($_SESSION['token']) || !isset($_SESSION['id'])) {
+        session_start();
+        $_SESSION['id'] = $user_obj->id;
+        $_SESSION['token'] = $token;
+        $user_obj->connected = True;
+        $user_obj->save();
+    }
+    $user_json = array(
+        "name"=>$user_obj->login,
+        "roles"=>$user_obj->roles,
+        "token"=>$token,
+        "home"=>"administration",
+        "id"=>$user_obj->id,
+        );
+    $app->response->headers->set('Content-Type', 'application/json');
+    $app->response->setBody(json_encode($user_json));
+});
+
+// Verification des cookies pour l'authentification
 function validateUserKey($uid, $key) {
-  // insert your (hopefully more complex) validation routine here
-  if ($uid == 'demo' && $key == 'demo') {
-    return true;
-  } else {
-    return false;
-  }
+    if ($uid == 'demo' && $key == 'demo') {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-// generates a temporary API key using cookies
-// call this first to gain access to protected API methods
-$app->get('/demo', function () use ($app) {    
+// Créer les cookies necessaire pour la session
+$app->get('/demo', function () use ($app) {
   try {
     $app->setEncryptedCookie('uid', 'demo', '5 minutes');
     $app->setEncryptedCookie('key', 'demo', '5 minutes');
@@ -50,23 +60,20 @@ $app->get('/demo', function () use ($app) {
     $app->response()->status(400);
     $app->response()->header('X-Status-Reason', $e->getMessage());
   }
-
 });
 
-
-
-$app->get('/admin/personnes', 'authenticate', function () use ($app) {
+$app->get('/admin/personnes', function () use ($app) {
     // On cherche les utilisateurs qui sont activé (enable à 1), ainsi que leur roles.
-    $users = Users::where('enable', 1)->with('roles')->get();
+    $users = Users::where('enabled', 1)->with('roles')->get();
     // On les envoie au format JSON pour que AngularJS les traites.
     $app->response->headers->set('Content-Type', 'application/json');
     $app->response->setBody(json_encode($users));
 });
 
 $app->get('/admin/personnes/:id', function($id) use ($app) {
-    $personne = Users::find($id)->with('roles')->get();
+    $personne = Users::where('id', $id)->with('roles')->firstOrFail();
+    $app->response->headers->set('Content-Type', 'application/json');
     $app->response->setBody(json_encode($personne));
-
 });
 
 $app->post('/admin/personnes', function() {
