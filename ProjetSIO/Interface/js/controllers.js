6b9ca594ced9988be3b85f87c6d0dce5b5fa8fc5
Guillaume Haag <haag.guillaume@gmail.com>
2015-11-21 12:20:09 +0100

    Implémentation calendrier (2)
    le calendrier se charge correctement maintenant, les indications sont en
    français aussi.
    Prochaines étapes : charger un calendrier public google pour les jours
    fériés + afficher les infos prises de la BDD

--- ProjetSIO/Interface/js/controllers.js
+++ ProjetSIO/Interface/js/controllers.js
@@ -203,17 +203,12 @@ webApp.controller('PlanPeriodesController',
 		$scope.eventSource = {
 				url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
 				className: 'gcal-event',           // an option!
-				currentTimezone: 'America/Chicago' // an option!
+				currentTimezone: 'Europe/Paris' // an option!
 		};
-		/* event source that contains custom events on the scope */
-		$scope.events = [
-		{title: 'All Day Event',start: new Date(y, m, 1)},
-		{title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-		{id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-		{id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-		{title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-		{title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-		];
+		
+		/* évènements pris de la BDD */
+		$scope.events = [];
+		
 		/* event source that calls a function on every view switch */
 		$scope.eventsF = function (start, end, timezone, callback) {
 		var s = new Date(start).getTime() / 1000;
@@ -304,15 +299,8 @@ webApp.controller('PlanPeriodesController',
 		};
 	
 		$scope.changeLang = function() {
-		if($scope.changeTo === 'Hungarian'){
-			$scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
-			$scope.uiConfig.calendar.dayNamesShort = ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo"];
-			$scope.changeTo= 'English';
-		} else {
-			$scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-			$scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-			$scope.changeTo = 'Hungarian';
-		}
+			$scope.uiConfig.calendar.dayNames = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+			$scope.uiConfig.calendar.dayNamesShort = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"];
 		};
 		/* event sources array*/
 		$scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
