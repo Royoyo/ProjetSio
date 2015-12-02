fbb6449c0393bbf4d4620e337754b406d960379d
Kevin-niva <niva.kevin@yahoo.fr>
2015-12-02 18:23:47 +0100

    maj

--- ProjetSIO/Backend/index.php
+++ ProjetSIO/Backend/index.php
@@ -45,20 +45,22 @@ $app->get('/send_inscription_mail/:id', function($id) use ($app, $mailer){
 */
 
 /**
-* Authentication
-* \param[in] role_required is a string wich is the title of the role the user must have
-* \return a boolean with the state \a True or an error code 
+* 				\b function
+* \brief 		Get the role the user must have and save it in #$authenticateWithRole variable
+* \param[in] 	$role_required is a string wich is the title of the role the user must have
+* \return 		a boolean with the state \a  True or an error code 
 */
-$authenticateWithRole = function ($role_required){
+$authenticateWithRole = function ($role_required){ 
     return function () use ( $role_required ) {
         $app = \Slim\Slim::getInstance();
         try{
             session_start();
-        } catch(Exception $e) {}		/// We are trying to open the users session
+        } catch(Exception $e) {}		/// We try to open the users session
         if (!isset($_SESSION['id'])) {	/// If the session ID isn't recognized, error 401 is sent
             $app->halt(401);	
         } else {						/// In this case, we check if the user id is correctly associated with his role, if not, error 401 is sent
             $id = $_SESSION['id'];
+            $user = Users::where('id', $id)->with('roles')->firstOrFail();	///	Matching the current role to the users id
             $user = Users::where('id', $id)->with('roles')->firstOrFail();	///	Matching the current role to the users id
             foreach($user->roles as $role) {
                 if ($role['role'] == $role_required) { /// Testing if the role is the role the user must have
