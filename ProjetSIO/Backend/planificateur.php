cb6781b12c2a9142f085ef93903145e0e620d377
Guillaume Haag <haag.guillaume@gmail.com>
2015-12-19 14:20:14 +0100

    Changement zone Planification - BACKEND
    Rajouts des routes manquantes
    TO DO : le traitement des dates n'est pas encore au point

--- ProjetSIO/Backend/planificateur.php
+++ ProjetSIO/Backend/planificateur.php
@@ -10,6 +10,10 @@
  * \details     this file contains all the routes for "planificateur" role
  */
  
+ //TO DO : revoir la logique appliquée au date
+ 
+ //Cours
+ 
 $app->get('/plan/cours/', $authenticateWithRole('planificateur'),  function () use ($app) {
     $start = $_GET['start'];
     $end = $_GET['end'];
@@ -67,6 +71,8 @@ $app->get('/plan/cours/:id', $authenticateWithRole('planificateur'), function ($
     $app->response->setBody(json_encode($cours));
 });
 
+//Enseignants
+
 $app->get('/plan/enseignant', $authenticateWithRole('planificateur'), function() use ($app) {
     $users = Users::whereHas('roles', function($q) {
         $q->where('role', 'enseignant');
@@ -75,9 +81,82 @@ $app->get('/plan/enseignant', $authenticateWithRole('planificateur'), function()
     $app->response->setBody(json_encode($users));
 });
 
+$app->get('/plan/enseignant/:id', $authenticateWithRole('planificateur'), function($id) use ($app) {
+    //TO DO : rajouter filtre enabled
+    $personne = Users::where('id', $id)->with('matieres')->select('id', 'firstName', 'lastName')->firstOrFail();
+    $app->response->headers->set('Content-Type', 'application/json');
+    $app->response->setBody(json_encode($personne));
+});
+
+$app->put('/plan/enseignant/:id', $authenticateWithRole('planificateur'), function($id) use ($app) {
+    try{
+        $json = $app->request->getBody();
+        $data = json_decode($json, true);
+        $personne = Users::where('id', $id)->with('matieres')->firstOrFail();
+        
+        //sync matieres
+        $newMatieres = [];
+        foreach($data['matieres'] as $matiere){
+            array_push($newMatieres, $matiere['id']);
+        }        
+        $personne->matieres()->sync($newMatieres);
+        
+        $app->response->setBody(true);
+    } catch(Exception $e) {
+        $app->response->headers->set('Content-Type', 'application/json');
+        $app->response->setBody(json_encode($e));
+    }
+});
+
+//Classes
+
 $app->get('/plan/classes', $authenticateWithRole('planificateur'), function() use ($app) {
     $classes = Classes::all();
     $app->response->headers->set('Content-Type', 'application/json');
     $app->response->setBody(json_encode($classes));
+});
+
+$app->get('/plan/classes/:id', $authenticateWithRole('planificateur'), function($id) use ($app) {
+    $classe = Classes::where('id', $id)->select('id', 'dateDebut', 'dateFin', 'nom')->firstOrFail();
+    $app->response->headers->set('Content-Type', 'application/json');
+    $app->response->setBody(json_encode($classe));
+});
+
+$app->put('/plan/classes/:id', $authenticateWithRole('planificateur'), function($id) use ($app) {
+    try{
+        $json = $app->request->getBody();
+        $data = json_decode($json, true);
+        $classe = Classes::where('id', $id)->firstOrFail();
+
+        $classe->nom = $data['nom'];
+        $classe->dateDebut = $data['dateDebut'];
+        $classe->dateFin = $data['dateFin'];
+        $classe->save();
+              
+        $app->response->setBody(true);
+    } catch(Exception $e) {
+        $app->response->headers->set('Content-Type', 'application/json');
+        $app->response->setBody(json_encode($e));
+    }
+});
+
+$app->post('/plan/classes', $authenticateWithRole('planificateur'), function() use ($app) {
+    try{
+        $json = $app->request->getBody();
+        $data = json_decode($json, true);
+        
+        $classe = new Classes(array(
+                'nom' => $data['nom'],
+                'dateDebut' => $data['dateDebut'],
+                'dateFin' => $data['dateFin']
+        ));
+        
+        $classe->save();
+              
+        $app->response->setBody(true);
+    } catch(Exception $e) {
+        $app->response->headers->set('Content-Type', 'application/json');
+        $app->response->setBody(json_encode($e));
+    }
 });
 ?>
