3ccf0eab65f9a040a0b0ca430fb061733f1f2394
Haag Guillaume <guillaume.haag.pro@gmail.com>
2016-04-05 22:40:06 +0200

    BACKEND rajout routes enseignants

--- ProjetSIO/Backend/enseignant.php
+++ ProjetSIO/Backend/enseignant.php
@@ -13,7 +13,7 @@
  
 //Cours
 $app->get('/ens/cours', $authenticateWithRole('enseignant'),  function () use ($app) {
-    $cours_obj = Cours::with('user', 'matiere', 'classes')->where('id_Users', $_SESSION['id'])->get();
+    $cours_obj = Cours::with('user', 'matiere', 'classes')->whereRaw('DATE(start) >= CURDATE() and id_Users = ?',[$_SESSION['id']])->get();
     $app->response->headers->set('Content-Type', 'application/json');
     $app->response->setBody(json_encode($cours_obj));
 });
@@ -34,28 +34,45 @@ $app->get('/ens/cours/:id', $authenticateWithRole('enseignant'),  function ($id)
 
 //Indisponibilités
 $app->get('/ens/indispo', $authenticateWithRole('enseignant'),  function () use ($app) {
-    $indispo = Indisponibilite::where('id_Users', $_SESSION['id'])->get();
+	//Suppression des indispos dans le passé à chaque requete ( à gérer autrement plus tard)
+	Indisponibilite::whereRaw('DATE(end) < CURDATE() and id_Users = ?',[$_SESSION['id']])->delete();
+
+    $indispos = Indisponibilite::where('id_Users', $_SESSION['id'])->get();
     $app->response->headers->set('Content-Type', 'application/json');
-    $app->response->setBody(json_encode($indispo));
+    $app->response->setBody(json_encode($indispos));
 });
 
-$app->post('/ens/indispo/', $authenticateWithRole('enseignant'),  function () use ($app) {
+$app->post('/ens/indispo', $authenticateWithRole('enseignant'),  function () use ($app) {
     $json = $app->request->getBody();
-    $data = json_decode($json, true);
-    
+    $data = json_decode($json, true);   
     $indispo = new Indisponibilite;
-    $indispo->start = $data['start'];
-    $indispo->end = $data['end'];
+    $indispo->start = new DateTime($data['start']);
+    $indispo->end = new DateTime($data['end']);
     $indispo->id_Users = $_SESSION['id'];
+
+	$duration = date_diff($indispo['start'],$indispo['end'],true);
+
+	//Si indispo d'une journée -> suppression de l'indispo précédente
+	if($duration->d < 2){
+		 Indisponibilite::whereRaw('DATE(start) = DATE(?) and id_Users = ?', [$data['start'], $_SESSION['id']])->delete();
+		 //$oldIndispo = Indisponibilite::where([
+		 //                                       ['id_Users',$data['user']['id']],
+		 //                                       ['DATE(start)',$data['start']->format('Y-m-d')]])->firstOrFail();
+   
+	//Si indispo de plus d'une journée -> période
+	} else {
+		//TO DO : Rajouter cas collision entre deux périodes
+		Indisponibilite::whereRaw('DATE(start) BETWEEN DATE(?) and DATE(?) and id_Users = ?', [$data['start'],$data['end'], $_SESSION['id']])->delete();
+	}
+
     $indispo->save();
-          
+
     $app->response->setBody(true);
 });
 
 $app->delete('/ens/indispo/:id', $authenticateWithRole('enseignant'),  function ($id) use ($app) {
     try {
-        $indispo = Indisponibilite::where('id_Users', $_SESSION['id'])->where('id', $id)->firstOrFail();
-        $indispo->delete();
+        $indispo = Indisponibilite::where('id_Users', $_SESSION['id'])->where('id', $id)->delete();
 
         $app->response->setBody(true);
     } catch(Exception $e) {
